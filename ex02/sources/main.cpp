7ecb2bc074da3428f51d9326b05efd65cd5214dd
Thibaud Maitre <thmaitre@z1r9p1.42lyon.fr>
2026-03-17 17:01:18 +0100

    added: base test on PresidentialPardonForm

--- ex02/sources/main.cpp
+++ ex02/sources/main.cpp
@@ -7,18 +7,76 @@ int main()
 {
 	try
 	{
-		Bureaucrat jhon("jhon", 2);
+		Bureaucrat jhon("jhon", 30);
 
-	   PresidentialPardonForm ppf("police");
+    PresidentialPardonForm ppf("police"); //sign 25, exec 5
 
-	   jhon.signForm(ppf);
-	   jhon.signForm(ppf);
-	   jhon.executeForm(ppf);
+    //cannot sign
+    jhon.signForm(ppf);
+    //cannot execute
+    jhon.executeForm(ppf);
 	}
-	 catch (std::exception & e)
-	 {
-	   std::cout << e.what() << "\n" << std::endl;
-	 }
+  catch (std::exception & e)
+  {
+    std::cout << e.what() << "\n" << std::endl;
+  }
+
+  std::cout << std::endl;
+
+	try
+	{
+		Bureaucrat jhon("jhon", 20);
+
+    PresidentialPardonForm ppf("police"); //sign 25, exec 5
+
+    //can sign
+    jhon.signForm(ppf);
+    //cannot execute
+    jhon.executeForm(ppf);
+	}
+  catch (std::exception & e)
+  {
+    std::cout << e.what() << "\n" << std::endl;
+  }
+
+  std::cout << std::endl;
+
+	try
+  {
+    Bureaucrat jhon("jhon", 2);
+
+    PresidentialPardonForm ppf("police"); //sign 25, exec 5
+
+    //can sign
+    jhon.signForm(ppf);
+    //already signed
+    jhon.signForm(ppf);
+    //can execute
+    jhon.executeForm(ppf);
+  }
+	catch (std::exception & e)
+  {
+    std::cout << e.what() << "\n" << std::endl;
+  }
+
+  std::cout << std::endl;
+
+	try
+  {
+    Bureaucrat jhon("jhon", 2);
+
+    PresidentialPardonForm ppf("police"); //sign 25, exec 5
+
+    //can sign
+    jhon.signForm(ppf);
+    //can execute
+    jhon.executeForm(ppf);
+  }
+	catch (std::exception & e)
+  {
+    std::cout << e.what() << "\n" << std::endl;
+  }
+
 }
 
 
